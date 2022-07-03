20dafd51e32f71762c6e2697bf5c48178cf0bbc4
Angelo Zammataro <juba.kem@gmail.com>
2022-07-03 20:05:11 +0200

    Secondo commit : Exercise - SQL-table

--- src/main/java/MyJDBC.java
+++ src/main/java/MyJDBC.java
@@ -1,21 +1,33 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyJDBC {
     public static void main(String[] args) {
 
-        try {
+        List<String>surnames = new ArrayList<>();
 
+        try {
+            //LOCAL
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb","developer","123456");
+            //REMOTE
+            //Connection connection = DriverManager.getConnection("jdbc:mysql://dakkar01.altervista.org:3306/my_dakkar01/newdb","dakkar01","");
             Statement statement = connection.createStatement();
+
+
             ResultSet resultSet = statement.executeQuery("select * from students");
+            System.out.println("Query for extract name and surname:");
                 while (resultSet.next()){
-                    System.out.println(resultSet.getString("first_name"));
+                    surnames.add(resultSet.getString("last_name"));
+                    System.out.println(resultSet.getString("first_name") + " - " +  resultSet.getString("last_name"));
                 }
-        }catch (Exception e){
+        }catch (SQLException e){
             e.printStackTrace();
         }
+        System.out.println("-----------------------------------");
+        System.out.println("All surname from arrayList:");
+        System.out.println(surnames);
+
+
     }
 }
